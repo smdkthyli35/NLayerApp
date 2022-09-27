d2a23880d5493db3fdf6c7b3217e12bc0aee2f59
Samed Kütahyalı <smdkthyli@gmail.com>
2022-09-27 17:52:31 +0300

    Repository - Entity Configurations added.

--- NLayer.Repository/Contexts/AppDbContext.cs
+++ NLayer.Repository/Contexts/AppDbContext.cs
@@ -17,5 +17,13 @@ namespace NLayer.Repository.Contexts
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
