82689e06d79aa1f2bbae182e16d69ad556bbcec5
Samed Kütahyalı <smdkthyli@gmail.com>
2022-11-19 15:48:30 +0300

    Repository - Model seeds added.

--- NLayer.Core/Models/BaseEntity.cs
+++ NLayer.Core/Models/BaseEntity.cs
@@ -10,6 +10,6 @@ namespace NLayer.Core.Models
     {
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
     }
 }
--- NLayer.Repository/Contexts/AppDbContext.cs
+++ NLayer.Repository/Contexts/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NLayer.Core.Models;
+using NLayer.Repository.Configurations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,10 @@ namespace NLayer.Repository.Contexts
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Entity<ProductFeature>().HasData(
+                new ProductFeature() { Id = 1, Color = "Kırmızı", Height = 100, Width = 200, ProductId = 1 },
+                new ProductFeature() { Id = 2, Color = "Mavi", Height = 300, Width = 500, ProductId = 2 });
 
             base.OnModelCreating(modelBuilder);
         }
